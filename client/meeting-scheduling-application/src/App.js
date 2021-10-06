ee9b088385a55f8a2cbf79641256bce2565d3655
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-06 19:36:49 +0300

    Implement rooms container styles

--- client/meeting-scheduling-application/src/App.js
+++ client/meeting-scheduling-application/src/App.js
@@ -1,10 +1,14 @@
 import ScheduleCalendar from './components/ScheduleCalendar';
+import Room from './components/Room';
 
 const App = () => {
     return (
         <div className="app">
-            <h1>Select a Date and Time</h1>
+            <h1>Select a Date and Room</h1>
             <ScheduleCalendar />
+            <div className="rooms-container">
+                <Room></Room>
+            </div>
         </div>
     );
 };
---
+++ client/meeting-scheduling-application/src/components/Room.js
@@ -0,0 +1,11 @@
+
+const Room = () => {
+    return (
+        <button>
+            <span>Name: Iskar</span>
+            <span>Capacity: 8</span>
+        </button>
+    );
+};
+
+export default Room;
