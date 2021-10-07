8f191dcb1ec02de5d99867c18f5e6b86fbd99691
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-07 12:23:23 +0300

    Create database context

---
+++ server/MeetingSchedulingApplication/MeetingSchedulingApplication/Data/MeetingSchedulingApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MeetingSchedulingApplication.Data
+{
+    public class MeetingSchedulingApplicationDbContext : DbContext
+    {
+        public MeetingSchedulingApplicationDbContext([NotNull] DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
