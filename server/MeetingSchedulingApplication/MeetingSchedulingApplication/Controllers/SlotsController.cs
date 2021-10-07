948a1d4e50208371664d6b9915a241ea14f11f92
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-07 12:48:12 +0300

    Implement room database model

---
+++ server/MeetingSchedulingApplication/MeetingSchedulingApplication/Controllers/SlotsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingSchedulingApplication.Controllers
+{
+    [Route("api/slots")]
+    public class SlotsController : ControllerBase
+    {
+    }
+}
---
+++ server/MeetingSchedulingApplication/MeetingSchedulingApplication/Data/Models/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeetingSchedulingApplication.Data.Models
+{
+    public class Room
+    {
+        [Required]
+        [Key]
+        public string Id { get; init; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string Name { get; init; }
+
+        public int Capacity { get; init; }
+
+        public TimeSpan AvailableFrom { get; init; }
+
+        public TimeSpan AvailableTo { get; init; }
+    }
+}
