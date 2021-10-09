7bb5b3ca7abbeac72b11250d290a79d69a639a45
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-09 23:30:05 +0300

    Implement auto seed rooms method

--- server/MeetingSchedulingApplication/MeetingSchedulingApplication/Extensions/ApplicationBuilderExtensions.cs
+++ server/MeetingSchedulingApplication/MeetingSchedulingApplication/Extensions/ApplicationBuilderExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using MeetingSchedulingApplication.Data;
+using MeetingSchedulingApplication.Data.Models;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MeetingSchedulingApplication.Extensions
@@ -14,17 +16,55 @@ namespace MeetingSchedulingApplication.Extensions
             using var serviceScope = app.ApplicationServices.CreateScope();
             var services = serviceScope.ServiceProvider;
 
-            MigrateDatabase(services);
+            var data = services
+                .GetRequiredService<MeetingSchedulingApplicationDbContext>();
+
+            MigrateDatabase(data);
+            SeedRooms(data);
 
             return app;
         }
 
-        private static void MigrateDatabase(IServiceProvider services)
-        {
-            var data = services
-                .GetRequiredService<MeetingSchedulingApplicationDbContext>();
+        private static void MigrateDatabase(MeetingSchedulingApplicationDbContext data)
+            => data?.Database.Migrate();
 
-            data?.Database.Migrate();
+        private static void SeedRooms(MeetingSchedulingApplicationDbContext data)
+        {
+            var rooms = data
+                .Rooms;
+
+            if (rooms.Any())
+            {
+                return;
+            }
+
+            rooms.AddRange(new()
+            {
+                Name = "Iskar",
+                Capacity = 8,
+                AvailableFrom = TimeSpan.Parse("09:00"),
+                AvailableTo = TimeSpan.Parse("18:00")
+            }, new()
+            {
+                Name = "Maritsa",
+                Capacity = 6,
+                AvailableFrom = TimeSpan.Parse("12:00"),
+                AvailableTo = TimeSpan.Parse("20:00")
+            }, new()
+            {
+                Name = "Danube",
+                Capacity = 12,
+                AvailableFrom = TimeSpan.Parse("12:00"),
+                AvailableTo = TimeSpan.Parse("20:00")
+            }, new()
+            {
+                Name = "Yantra",
+                Capacity = 4,
+                AvailableFrom = TimeSpan.Parse("09:00"),
+                AvailableTo = TimeSpan.Parse("18:00")
+            });
+
+            data.SaveChanges();
         }
     }
 }
